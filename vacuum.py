b1d691c21df1c390d1b0fc82a22d3e6668d731cb
Unnati <unnatirastogi22@gmail.com>
2026-03-09 20:41:04 +0530

    Vacuum initial add

---
+++ vacuum.py
@@ -0,0 +1,39 @@
+# Simple Reflex Agent for Vacuum Cleaner World
+
+def reflex_vacuum_agent(location, status):
+    if status == "Dirty":
+        return "Suck"
+    elif location == "A":
+        return "Right"
+    elif location == "B":
+        return "Left"
+
+# Taking user input for environment
+environment = {}
+
+environment["A"] = input("Enter status of location A (Dirty/Clean): ")
+environment["B"] = input("Enter status of location B (Dirty/Clean): ")
+
+# Initial location of the vacuum
+location = input("Enter initial location of vacuum (A/B): ")
+
+# Run the agent for 5 steps
+for step in range(5):
+    status = environment[location]
+
+    action = reflex_vacuum_agent(location, status)
+
+    print("\nStep:", step + 1)
+    print("Location:", location)
+    print("Status:", status)
+    print("Action:", action)
+
+    # Update environment
+    if action == "Suck":
+        environment[location] = "Clean"
+
+    elif action == "Right":
+        location = "B"
+
+    elif action == "Left":
+        location = "A"
